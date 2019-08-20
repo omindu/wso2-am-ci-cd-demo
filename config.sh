d10a18ec835ba30cc8b5b7fd35b49b8ee19be3d1
omindu <omindu@wso2.com>
2019-08-20 07:05:54 +0200

    Test update

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 echo $(which apimcli)
 apimcli version
 
-rm -rf $HOME/.wso2apimcli
+#rm -rf $HOME/.wso2apimcli
 
 echo 'setting up test environment'
 apimcli add-env -n test \
@@ -15,16 +15,16 @@ apimcli add-env -n test \
                     --api_list https://localhost:9444/api/am/publisher/v0.14/apis \
                     --app_list https://localhost:9444/api/am/store/v0.14/applications
 echo 'logging into test'
-apimcli login test -u $USERNAME -p $PASSWORD -k
+apimcli login test -u a$USERNAME -p $PASSWORD -k
 
-echo 'setting up prod environment'
-apimcli add-env -n prod \
-                    --registration https://localhost:9445/client-registration/v0.14/register \
-                    --apim https://localhost:9445 \
-                    --token https://localhost:8245/token \
-                    --import-export https://localhost:9445/api-import-export-2.6.0-v10 \
-                    --admin https://localhost:9445/api/am/admin/v0.14 \
-                    --api_list https://localhost:9445/api/am/publisher/v0.14/apis \
-                    --app_list https://localhost:9445/api/am/store/v0.14/applications
-echo 'logging into prod'
-apimcli login prod -u $USERNAME -p $PASSWORD -k
+echo 'setting up dev environment'
+apimcli add-env -n dev \
+                    --registration https://localhost:9443/client-registration/v0.14/register \
+                    --apim https://localhost:9443 \
+                    --token https://localhost:8243/token \
+                    --import-export https://localhost:9443/api-import-export-2.6.0-v10 \
+                    --admin https://localhost:9443/api/am/admin/v0.14 \
+                    --api_list https://localhost:9443/api/am/publisher/v0.14/apis \
+                    --app_list https://localhost:9443/api/am/store/v0.14/applications
+echo 'logging into dev'
+apimcli login dev -u $USERNAME -p $PASSWORD -k
